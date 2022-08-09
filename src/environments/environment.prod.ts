d1faa3dd7dfd05d81b7a73dc2ea8394d4245fc98
dialv <dialvultimate@gmail.com>
2022-08-09 10:52:27 -0600

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'test-api-dialv',
+    appId: '1:156254342867:web:06821472f40c2619388e26',
+    storageBucket: 'test-api-dialv.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBJonEDmgkFVlbL55keDiqRy7epmCj10S0',
+    authDomain: 'test-api-dialv.firebaseapp.com',
+    messagingSenderId: '156254342867',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'test-api-dialv',
+    appId: '1:156254342867:web:06821472f40c2619388e26',
+    storageBucket: 'test-api-dialv.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBJonEDmgkFVlbL55keDiqRy7epmCj10S0',
+    authDomain: 'test-api-dialv.firebaseapp.com',
+    messagingSenderId: '156254342867',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
